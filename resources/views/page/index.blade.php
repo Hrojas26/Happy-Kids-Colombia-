c3bd6602eb5ac05b7a4f0e36630272d1be0a2ea0
Heisemberthr <hrojas26@uan.edu.co>
2024-04-21 19:55:34 -0500

    se crea slider principal y carpeta en public para las imagenes

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -14,7 +14,37 @@
 <body>
 @include('components.headerPage')
 
-    <!-- Ojo mario, contenido estatico -->
+{{-- SLIDER --}}
+
+<div class="container">
+    <div class="row">
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000" data-bs-wrap="true">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="{{ asset('img/1.jpg') }}" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="{{ asset('img/2.jpg') }}" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="{{ asset('img/3.jpg') }}" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+         </div>
+    </div>
+
+</div>
+
+{{-- END SLIDER --}}
+<!-- Ojo mario, contenido estatico -->
 
 @include('components.footerPage')
 <script src="{{ asset('js/bootstrap.js') }}"></script>
