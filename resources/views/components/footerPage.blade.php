00b7c18b024f215e16ddc340d22613886fe2f1ec
Heisemberthr <hrojas26@uan.edu.co>
2024-04-29 20:32:11 -0500

    footerListo

--- resources/views/components/footerPage.blade.php
+++ resources/views/components/footerPage.blade.php
@@ -1,5 +1,23 @@
-<div>
-    <footer>
-        <p>Todos los derechos reservados <span>{{ date('d/m/Y') }}</span></p>
+
+
+<div class="container-fluid footer">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+      <div class="col-md-4 d-flex align-items-center">
+        <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+            <a class="ms-3 text-body-secondary" href="#"><img src="{{ asset('img/hkc.png') }}" alt="Happy Kids Colombia" width="35" height="15"></a>
+          <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
+        </a>
+        <span class="mb-3 mb-md-0 text-body-secondary">© 2024 Happy Kids Colombia</span>
+
+      </div>
+
+      <ul class="nav col-md-4 justify-content-center list-unstyled d-flex">
+
+
+        <li class="ms-3"><a class="text-body-secondary" href="#"><img src="{{ asset('img/facebook.png') }}" alt="facebook" width="35" height="38"></a></li>
+        <li class="ms-3"><a class="text-body-secondary" href="#"><img src="{{ asset('img/instagram.png') }}" alt="Instagram" width="35" height="38"></a></li>
+        <li class="ms-3"><a class="text-body-secondary" href="#"><img src="{{ asset('img/ws.png') }}" alt="whatsapp" width="35" height="38"></a></li>
+
+      </ul>
     </footer>
-</div>
+  </div>
